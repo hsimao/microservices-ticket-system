92aa9710db10be1c45d93e52d627cbf2e728f68d
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-05-17 13:57:05 +0800

    feat: auth mongo basic setting

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors';
 import {
@@ -24,6 +25,18 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Auth service listening on port 3000!');
-});
+const start = async () => {
+  try {
+    // 連接 k8s pod 的 mongo service
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Auth service listening on port 3000!');
+  });
+};
+
+start();
