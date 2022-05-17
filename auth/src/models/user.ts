780fcb299ae53787cf0e3ebb70489edfb7c04739
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-05-17 15:09:02 +0800

    feat: auth user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// NOTE: 擴充 user Model 屬性
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// NOTE: 擴充創建 User 實例返回的 Document 屬性
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// NOTE: 將 typescript interface 綁定到 mongo 實例靜態方法上
+// DEMO: User.build({ email: 'email@gmail.com', password: '1234'})
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
