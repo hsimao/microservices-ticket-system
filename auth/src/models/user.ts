cef735770a2f6705f85c8526178bba81459768ab
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-05-17 16:13:41 +0800

    feat: user password to hash

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 interface UserAttrs {
   email: string;
@@ -25,6 +26,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 // NOTE: 將 typescript interface 綁定到 mongo 實例靜態方法上
