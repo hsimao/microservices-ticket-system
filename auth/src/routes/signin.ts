551f4241dd7ca1babbe8ba04662c492da5aff37f
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-05-19 19:44:52 +0800

    feat: auth validate request middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,19 @@
-import express from "express";
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post("/signin", (req, res) => {
-  res.send("Hi there!");
-});
+router.post(
+  '/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('You must suppy a password'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send('Hi there!');
+  }
+);
 
 export default router;
