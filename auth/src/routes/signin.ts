c047a64323a2d8673bf427c6dd275a26c8bde1b8
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-05-19 19:58:15 +0800

    feat: auth signin logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { body } from 'express-validator';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors';
+import { Password } from '../services/password';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -11,8 +15,38 @@ router.post(
     body('password').trim().notEmpty().withMessage('You must suppy a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.send('Hi there!');
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      // @ts-ignore
+      process.env.JWT_KEY
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
   }
 );
 
