35626d62741e953d14de7800d4c01a16068f9050
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-05-19 20:26:02 +0800

    feat: auth current user logic

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
-import express from "express";
+import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get("/currentuser", (req, res) => {
-  res.send("Hi there!");
+router.get('/currentuser', (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    // @ts-ignore
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export default router;
