4641e4d42774a6d90f65404c6215f3df63cf64d0
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-06-05 14:24:18 +0800

    feat: auth global gignup helper function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,15 +2,8 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about the current user', async () => {
-  const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201);
+  const cookie = await global.signup();
 
-  const cookie = authResponse.get('Set-Cookie');
   const response = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
@@ -18,4 +11,13 @@ it('responds with details about the current user', async () => {
     .expect(200);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
