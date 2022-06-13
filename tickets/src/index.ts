91677bc7f0cd4ac52e2ca237a1643a65a1df2ccc
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-06-13 19:22:14 +0800

    feat: tickets、auth mongo URI use env

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,9 +7,13 @@ const start = async () => {
     throw new Error('JWT_KEY must be defined');
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   try {
     // 連接 k8s pod 的 mongo service
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err);
