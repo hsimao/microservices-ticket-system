69d3885198462a67295b844ed8b1e5ed27614344
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-07-02 23:10:52 +0800

    feat: nats 封裝 base listener class and ticket crated listener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -15,27 +16,7 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true) // 手動判斷是否接收成功，若失敗將會在 30秒(預設) 後自動找另外的 queue 發送, 直到成功
-    .setDeliverAllAvailable() // 每次重啟 listen port 都會重新執行之前執行過的所有 subscrip 任務
-    .setDurableName('accounting-service'); // 將會記錄訂閱狀態，有發出去成功後會標記，將在下次重啟時不用在重新發送, 需搭配 setDeliverAllAvailable 一起
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'orders-service-queue-group',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
