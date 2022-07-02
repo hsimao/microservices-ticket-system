36a860d5daf397b3d305c47bfc16a3ff562ca5b5
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-07-02 22:41:30 +0800

    feat: nats listener setting service fail, reload event

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,7 +15,12 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true) // 手動判斷是否接收成功，若失敗將會在 30秒(預設) 後自動找另外的 queue 發送, 直到成功
+    .setDeliverAllAvailable() // 每次重啟 listen port 都會重新執行之前執行過的所有 subscrip 任務
+    .setDurableName('accounting-service'); // 將會記錄訂閱狀態，有發出去成功後會標記，將在下次重啟時不用在重新發送, 需搭配 setDeliverAllAvailable 一起
+
   const subscription = stan.subscribe(
     'ticket:created',
     'orders-service-queue-group',
