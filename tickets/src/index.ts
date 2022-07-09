2df00da1f0368b1f4b7686d190bc251cc808a8e0
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-07-09 15:10:13 +0800

    feat: tickets nats graceful shutdown

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,13 @@ const start = async () => {
 
   try {
     await natsWrapper.connect('ticketing', 'sdfjs', 'http://nats-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     // 連接 k8s pod 的 mongo service
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
