f6e2dfa2ff1e3814aae2767b30398967e7e2b04f
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-07-13 21:06:41 +0800

    feat: orders add ticket model

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@marschen-tickets/common';
+import { TicketDoc } from './ticket';
 interface OrderAttrs {
   userId: string;
   status: OrderStatus;
