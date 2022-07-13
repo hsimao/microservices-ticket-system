80f863d73c3341ef80915a556997ea2cc58d745f
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-07-13 20:24:13 +0800

    feat: orders models

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@marschen-tickets/common';
 
 const router = express.Router();
 
-router.post('/', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export default router;
