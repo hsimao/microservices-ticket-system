9ff694c95a85c178be59216de8981cb9155b2d5b
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-07-15 20:13:35 +0800

    feat: orders show list api logic

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,4 +1,4 @@
 export { default as createOrderRouter } from './new';
 export { default as showOrderRouter } from './show';
-export { default as showListOrderRouter } from './showList';
+export { default as showListOrderRouter } from './show-list';
 export { default as deleteOrderRouter } from './delete';
--- orders/src/routes/showList.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-
-const router = express.Router();
-
-router.get('/:id', async (req: Request, res: Response) => {
-  res.send({});
-});
-
-export default router;
